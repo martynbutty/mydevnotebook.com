33eaafc2de95b2780428f6e24439f64bc1bd0429
martyn <martynbutty@googlemail.com>
2021-12-15 18:57:21 +0000

    Fix footer and add sidebar menu link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -58,7 +58,7 @@ const config = {
       },
       footer: {
         style: 'dark',
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} MyDevNotebook.com. Built with Docusaurus.`,
       },
       prism: {
         theme: lightCodeTheme,
