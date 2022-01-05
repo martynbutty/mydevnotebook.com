085f2557e4d53872b94baa609c5984245c80c1f0
martynbutty <martynbutty@googlemail.com>
2022-01-05 15:05:45 +0000

    oops, don't need script tags inside a script - vcopy paste honest :)

--- static/ga.js
+++ static/ga.js
@@ -1,7 +1,5 @@
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
 
-  gtag('config', 'G-H8PHRNFHKD');
-</script>
+gtag('config', 'G-H8PHRNFHKD');
